1ec2bd01e19ccea8c555db7bebfaf5c6ca9ccc60
elisiothemutt <fujoshithe@gmail.com>
2026-07-07 15:48:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const mensagem = "* teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste.";
+const mensagem = "* teste teste teste teste teste teste teste teste teste teste teste teste teste.";
 
 const texto = document.getElementById("text");
 
 let indice = 0;
-let escrevendo = true;
 
 function escrever() {
 
     if (indice >= mensagem.length) {
-        escrevendo = false;
         return;
     }
 
     texto.textContent += mensagem[indice];
+
     indice++;
 
-    // velocidade parecida com Undertale
-    setTimeout(escrever, 28);
+    setTimeout(escrever, 30);
 
 }
 
